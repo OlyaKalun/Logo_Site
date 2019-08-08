8c8a016266dece3a926e3fb8dd54d18291404cc9
unknown <olyakalun1998@gmail.com>
2019-08-09 02:58:58 +0300

    adaptation and fixed bugs

--- js/refresh.js
+++ js/refresh.js
@@ -1,26 +1,9 @@
-// var intest = null;
 
-// $('.box-shadow-block5').on('click', function(){
-//     appendValue = '<div class="nav-icon-shadow "><div class=" box-shadow-block5 mx-auto"><img src="img/block5_1.png" class="picture-block5" alt="block5_1"></div></div>';
-//     self= this; // box-shadow-block5
-//     inHtml =  $(self).children().eq(0).html();
-//     intest = inHtml;
-//     $(self).children().eq(0).empty().append(appendValue);
-    
-// });
-
-// $('.box-shadow-block5').on('click', function(){
-//     appendValue = intest;
-//     self = this;
-//     $(self).children().eq(0).empty().append(appendValue);
-// });
-
-
-// $('.box-shadow-block5').animate({ 
-//     boxShadow: "0px 0px 5px 3px hsla(100, 70%, 60%, 0.8)"
-//   }); 
-
-$('.box-shadow-block5').on('mouseenter', function(){
-$('.box-shadow-block5').stop().animate({boxShadow: '0 0 30px #9ca6ae'});
-
+$( ".shadow-block5" ).mouseover(function() {
+    jQuery(this).stop().animate({boxShadow: '0 0 30px #9ca6ae'})
 });
+
+$( ".shadow-block5" ).mouseout(function() {
+    jQuery(this).stop().animate({boxShadow: '0 0 10px #caced1'})
+});
+
